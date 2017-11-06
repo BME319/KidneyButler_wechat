c849ed626045e0fd28b47f10ab1efed9834e9d5d
BME319 <zjubme319@163.com>
2017-11-06 10:26:45 +0800

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -33,13 +33,13 @@ angular.module('kidney.services', ['ionic', 'ngResource'])
   // imgLargeUrl: 'http://appmediaservice.haihonghospitalmanagement.com/uploads/photos/',
   // 测试服务器地址
   // version2Url: 'http://121.43.107.106:4060/api/v2/',
-  baseUrl: 'https://application.haihonghospitalmanagement.com/api/v2/',
-  urineConnectUrl: 'https://application.haihonghospitalmanagement.com/',
-  mediaUrl: 'https://media.haihonghospitalmanagement.com/',
+  baseUrl: 'https://docker2.haihonghospitalmanagement.com/api/v2/',
+  urineConnectUrl: 'https://docker2.haihonghospitalmanagement.com/',
+  mediaUrl: 'https://df2.haihonghospitalmanagement.com/',
   // mediaUrl: 'http://121.43.107.106:8054/',
-  socketServer: 'https://application.haihonghospitalmanagement.com/',
-  imgThumbUrl: 'https://media.haihonghospitalmanagement.com/uploads/photos/resize',
-  imgLargeUrl: 'https://media.haihonghospitalmanagement.com/uploads/photos/',
+  socketServer: 'https://docker2.haihonghospitalmanagement.com/',
+  imgThumbUrl: 'https://df2.haihonghospitalmanagement.com/uploads/photos/resize',
+  imgLargeUrl: 'https://df2.haihonghospitalmanagement.com/uploads/photos/',
   //
   NiaodaifuUrl: 'https://open.niaodaifu.cn/wap/login',
 
